d823875a97657d36e976f98551c8dbe4a65207e8
peter kimanzi <kimjayden001@gmail.com>
2022-05-11 16:23:01 +0300

    my first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-container', {
+  direction: 'vertical',
+  mousewheel: {},
+  effect: 'cube',
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false
+  }
+});
